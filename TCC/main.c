80de0dcef075886b104af64f0eef73292b5a19e2
rgrosko <rafael.grosko1@gmail.com>
2016-08-31 20:51:33 -0300

    refatoramento

--- TCC/main.c
+++ TCC/main.c
@@ -46,8 +46,16 @@
 #define DISABLED    0x08
 
 
+int32_t rede, bateria = 0;
+int index;
+char* recebeDadosBluetooth;
 uint8_t tempo_passado;
 uint16_t pulsos_contados;
+REFTEMPO referencia;
+uint8_t date[6] = {0x00,0x00,0x00,0x00,0x00,0x00};
+uint8_t modo_atual = START, aberta = 0x00;
+uint16_t leituras_salvas= 0;
+const uint32_t halfseg = 1666666;//833333;
 
 void GPIODIntHandler(void) {
 	GPIOIntClear(GPIO_PORTD_BASE, GPIO_INT_PIN_1);
@@ -57,10 +65,7 @@ void GPIODIntHandler(void) {
 }
 /************************************************/
 
-int32_t rede, bateria = 0;
-int index;
-char* recebeDadosBluetooth;
-const uint32_t halfseg = 1666666;//833333;
+
 
 void GPIO_Init() {
 	//INPUTS: PB2 - ENABLE READ / PD1 - COUNT
@@ -144,14 +149,181 @@ void ShowDateTime() {
 	LCD_WriteCol(imprime, 0, 12);
 }
 
+void EnviaValorAtual(const REFTEMPO* referencia) {
+	LCD_Clear();
+
+	LCD_Write("       ENVIA", 0);
+	LCD_Write("       ULTIMO", 1);
+	LCD_Write("       VALOR", 2);
+
+	DADOMEDIDA ultimo;
+	uint16_t mes_atual = referencia->end_proxmes.word - 0x1173; //TODO revisar
+	if (referencia->end_diaria.word - 3 != mes_atual)
+	ultimo = EEPROM_PegaLeitura(referencia->end_diaria.word - 6);
+	else
+	ultimo.med.word = 0;
+
+	Bluetooth_EnviaMedicao(ultimo.med.word);
+
+	Delay(1000);
+	LCD_Clear();
+}
+
+void EnviaMediaAnual(const REFTEMPO* referencia) {
+	LCD_Clear();
+
+	LCD_Write("       ENVIA", 0);
+	LCD_Write("       MEDIA ", 1);
+	LCD_Write("       ANUAL", 2);
+
+	DADOANUAL media;
+	media = EEPROM_PegaMedia(referencia->end_proxano.word - 9);
+	uint32_t valor = ((media.acu_hi.word * 65536) + media.acu_lo.word)
+			/ media.cnt_ms.word;
+	Bluetooth_EnviaMedicao(valor);
+
+	Delay(1000);
+	LCD_Clear();
+}
+
+void EnviaHistorico(){
+	LCD_Clear();
+
+	LCD_Write("       ENVIA ", 1);
+	LCD_Write("     HISTORICO", 2);
+
+	B16 cal;
+	uint16_t mem_mes = MES1;
+	uint8_t day[3];
+	uint8_t hour[3];
+	DADOMEDIDA hist;
+	int ind_mes;
+	for(ind_mes = 0; ind_mes < 6; ind_mes++) {
+		mem_mes = mem_mes + (ind_mes*0x1173);
+		cal = GetMesAno(mem_mes);
+		int ler;
+		for(ler = 0; ler < 744; ler++) {
+			hist = LeSequencia((ind_mes*0x1173), ler);
+			day[0] = hist.datetime[0];
+			day[1] = cal.byte[1];
+			day[2] = cal.byte[0];
+			hour[0] = hist.datetime[1];
+			hour[1] = hist.datetime[2];
+			hour[2] = hist.datetime[3];
+			Bluetooth_EnviaDados(hist.med.word, day, hour);
+		}
+	}
+
+	Delay(1000);
+	LCD_Clear();
+}
+
+void ResetMemoria() {
+	LCD_Clear();
+
+	LCD_Write("        RESET", 1);
+	LCD_Write("       MEMORIA", 2);
+
+	ResetMem();
+
+	LCD_Clear();
+}
+
+void AbreValvula() {
+	LCD_Clear();
+
+	OpenValve();
+
+	LCD_Write("    ABRE VALVULA", 0);
+	LCD_Write("---------/ /--------", 2);
+
+	Delay(1000);
+	LCD_Clear();
+}
+
+void FechaValvula() {
+	LCD_Clear();
+
+	CloseValve();
+
+	LCD_Write("   FECHA VALVULA", 0);
+	LCD_Write("--------------------", 2);
+
+	Delay(1000);
+	LCD_Clear();
+}
+
+void AjustaHora(){
+	LCD_Clear();
+
+	LCD_Write("  RECEBENDO DADOS", 0);
+
+	//recebe data
+	char imprime[15];
+	char auxData[6] = "999999";
+	Delay(1000);
+	while(auxData[0] == '9'){
+		Delay(200);
+		recebeDadosBluetooth = Bluetooth_RecebeDados();
+		for(index = 0; index < 6; index++){
+			auxData[index] = recebeDadosBluetooth[index];
+		}
+	}
+	date[3] = ((auxData[0] - 0x30 ) * 10) + (auxData[1] - 0x30);
+	date[4] = ((auxData[2] - 0x30 ) * 10) + (auxData[3] - 0x30);
+	date[5] = ((auxData[4] - 0x30 ) * 10) + (auxData[5] - 0x30);
+
+	sprintf(imprime, "DATA %02d/%02d/%02d", date[3], date[4], date[5]);
+	LCD_Write(imprime, 1);
+
+	//recebe hora
+	char auxTime[6] = "999999";
+	Delay(1000);
+	while(auxTime[0] == '9'){
+		Delay(200);
+		recebeDadosBluetooth = Bluetooth_RecebeDados();
+		for(index = 0; index < 6; index++){
+			auxTime[index] = recebeDadosBluetooth[index];
+		}
+	}
+	date[2] = ((auxTime[0] - 0x30 ) * 10) + (auxTime[1] - 0x30);
+	date[1] = ((auxTime[2] - 0x30 ) * 10) + (auxTime[3] - 0x30);
+	date[0] = ((auxTime[4] - 0x30 ) * 10) + (auxTime[5] - 0x30);
+
+	sprintf(imprime, "HORA %02d:%02d:%02d", date[2], date[1], date[0]);
+	LCD_Write(imprime, 2);
+
+	Delay(1000);
+
+	//HORA - MINUTO - SEGUNDO
+	DS1307_SetTime(date[2],date[1],date[0]);
+	//DIA - MES - ANO
+	DS1307_SetDate(date[3],date[4],date[5]);
+
+	LCD_Clear();
+}
+
+void ImprimeVolts(){
+	LCD_Clear();
+
+	LCD_Write("FONTES DE ENERGIAS", 0);
+
+	char imprime[10];
+
+	float volt = bateria / 1.67;
+	sprintf(imprime, "BATERIA = %.2f V", volt); // 9.0v -> 2.18V
+	LCD_Write(imprime, 1);
+
+	volt = rede / 2;
+	sprintf(imprime, "REDE = %.2f V", volt); //12.4V -> 2.80V
+	LCD_Write(imprime, 2);
+
+	Delay(1000);
+	LCD_Clear();
+}
+
  void main(void) {
 	Inicia_Tiva();
-
-	REFTEMPO referencia;
-	uint8_t date[6] = {0x00,0x00,0x00,0x00,0x00,0x00};
-	uint8_t modo_atual = START, aberta = 0x00;
-	uint16_t leituras_salvas= 0;
-
 
 	LCD_BlackLight_Enable();
 	LCD_Clear();
@@ -177,150 +349,35 @@ void ShowDateTime() {
 		comando = recebeDadosBluetooth[5];
 		if(comando == '1'){
 	//	if( strcmp(temp, "atual") == 0 ){
-			LCD_Clear();
-			LCD_Write("       ENVIA", 0);
-			LCD_Write("       ULTIMO", 1);
-			LCD_Write("       VALOR", 2);
-
-			DADOMEDIDA ultimo;
-			uint16_t mes_atual = referencia.end_proxmes.word - 0x1173; //TODO revisar
-			if(referencia.end_diaria.word - 3 != mes_atual)
-				ultimo = EEPROM_PegaLeitura(referencia.end_diaria.word - 6);
-			else
-				ultimo.med.word = 0;
-			Bluetooth_EnviaMedicao(ultimo.med.word);
-
-			Delay(1000);
-			LCD_Clear();
+			EnviaValorAtual(&referencia);
 		}
 		if(comando == '2'){
 	//	if( strcmp(temp, "anter") == 0 ){
-			LCD_Clear();
-			LCD_Write("       ENVIA", 0);
-			LCD_Write("       MEDIA ", 1);
-			LCD_Write("       ANUAL", 2);
-
-			DADOANUAL media;
-			media = EEPROM_PegaMedia(referencia.end_proxano.word - 9);
-            uint32_t valor = ((media.acu_hi.word*65536) + media.acu_lo.word)/media.cnt_ms.word;
-			Bluetooth_EnviaMedicao(valor);
-
-			Delay(1000);
-			LCD_Clear();
+			EnviaMediaAnual(&referencia);
 		}
  		if(comando == '3'){
 	//	if( strcmp(temp, "histo") == 0 ){
-			LCD_Clear();
-			LCD_Write("       ENVIA ", 1);
-			LCD_Write("     HISTORICO", 2);
-
-			B16 cal;
-			uint16_t mem_mes = MES1;
-			uint8_t day[3];
-			uint8_t hour[3];
-			DADOMEDIDA hist;
-			int ind_mes;
-			for(ind_mes = 0; ind_mes < 6; ind_mes++) {
-				mem_mes = mem_mes + (ind_mes*0x1173);
-				cal = GetMesAno(mem_mes);
-				int ler;
-				for(ler = 0; ler < 744; ler++) {
-					hist = LeSequencia((ind_mes*0x1173), ler);
-					day[0] = hist.datetime[0];
-					day[1] = cal.byte[1];
-					day[2] = cal.byte[0];
-					hour[0] = hist.datetime[1];
-					hour[1] = hist.datetime[2];
-					hour[2] = hist.datetime[3];
-					Bluetooth_EnviaDados(hist.med.word, day, hour);
-				}
-			}
-
-			Delay(1000);
-			LCD_Clear();
+			EnviaHistorico();
 		}
 		if(comando == '4'){
 	//	if( strcmp(temp, "reset") == 0 ){
-			LCD_Clear();
-			LCD_Write("        RESET", 1);
-			LCD_Write("       MEMORIA", 2);
-			ResetMem();
+			ResetMemoria();
 		}
 		if(comando == '5'){
 	//	if( strcmp(temp, "ajust") == 0 ){
-			LCD_Clear();
-			LCD_Write("  RECEBENDO DADOS", 0);
-			//recebe data
-			char imprime[15];
-			char auxData[6] = "999999";
-			Delay(1000);
-			while(auxData[0] == '9'){
-				Delay(200);
-				recebeDadosBluetooth = Bluetooth_RecebeDados();
-				for(index = 0; index < 6; index++){
-					auxData[index] = recebeDadosBluetooth[index];
-				}
-			}
-			date[3] = ((auxData[0] - 0x30 ) * 10) + (auxData[1] - 0x30);
-			date[4] = ((auxData[2] - 0x30 ) * 10) + (auxData[3] - 0x30);
-			date[5] = ((auxData[4] - 0x30 ) * 10) + (auxData[5] - 0x30);
-			sprintf(imprime, "DATA %02d/%02d/%02d", date[3], date[4], date[5]);
-			LCD_Write(imprime, 1);
-			//recebe hora
-			char auxTime[6] = "999999";
-			Delay(1000);
-			while(auxTime[0] == '9'){
-				Delay(200);
-				recebeDadosBluetooth = Bluetooth_RecebeDados();
-				for(index = 0; index < 6; index++){
-					auxTime[index] = recebeDadosBluetooth[index];
-				}
-			}
-			date[2] = ((auxTime[0] - 0x30 ) * 10) + (auxTime[1] - 0x30);
-			date[1] = ((auxTime[2] - 0x30 ) * 10) + (auxTime[3] - 0x30);
-			date[0] = ((auxTime[4] - 0x30 ) * 10) + (auxTime[5] - 0x30);
-			sprintf(imprime, "HORA %02d:%02d:%02d", date[2], date[1], date[0]);
-			LCD_Write(imprime, 2);
-
-			Delay(1000);
-
-			//HORA - MINUTO - SEGUNDO
-			DS1307_SetTime(date[2],date[1],date[0]);
-			//DIA - MES - ANO
-			DS1307_SetDate(date[3],date[4],date[5]);
-			LCD_Clear();
+			AjustaHora();
 		}
 		if(comando == '6'){
 	//	if( strcmp(temp, "abrir") == 0 ){
-			LCD_Clear();
-			OpenValve();
-			LCD_Write("    ABRE VALVULA", 0);
-			LCD_Write("---------/ /--------", 2);
-			Delay(1000);
-			LCD_Clear();
+			AbreValvula();
 		}
 		if(comando == '7'){
 	//	if( strcmp(temp, "fecha") == 0 ){
-			LCD_Clear();
-			CloseValve();
-			LCD_Write("   FECHA VALVULA", 0);
-			LCD_Write("--------------------", 2);
-			Delay(1000);
-			LCD_Clear();
+			FechaValvula();
 		}
 		if(comando == '8'){
 	//	if( strcmp(temp, "volts") == 0 ){
-			LCD_Clear();
-			LCD_Write("FONTES DE ENERGIAS", 0);
-			char imprime[10];
-			float volt = bateria / 1.67;
-			sprintf(imprime, "BATERIA = %.2f V", volt); // 9.0v -> 2.18V
-			LCD_Write(imprime, 1);
-			volt = rede / 2;
-			sprintf(imprime, "REDE = %.2f V", volt); //12.4V -> 2.80V
-			LCD_Write(imprime, 2);
-			Delay(1000);
-			LCD_Clear();
+			ImprimeVolts();
 		}
 
 		ShowDateTime();
